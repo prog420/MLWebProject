8aad9a812d3f6a1c15af7f8830354b89e46c9376
prog420 <dmitry.b.v.work@gmail.com>
2021-05-23 23:35:23 +0300

    Web Translation API  (Systran.io) added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import Algorithmia
 from CGRtools import smiles
 from pubchempy import Compound, get_compounds
@@ -38,13 +39,30 @@ def image():
 
 @app.route('/smiles', methods=['GET','POST'])
 def get_smiles():
+    """
+    Get SMILES string for IUPAC names
+    Web translation API: SYSTRAN.io
+    """
     smiles_list = []
-
     string = request.args.get('post')
+    string = string.replace(",", ".")
     string = string.split(".")
-    for element in string:
+    source_lang = request.args.get('lang')
+    url = "https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/translation/text/translate"
+    headers = {
+    'x-rapidapi-key': "28bb9cac94msh0d21dd8efbc884ep1172f0jsndc7797830a0e",
+    'x-rapidapi-host': "systran-systran-platform-for-language-processing-v1.p.rapidapi.com"
+    }
+
+    for i in range(len(string)):
+        if source_lang != "en":
+            # If source lang. is not English, perform translation
+            querystring = {"source": source_lang, "target":"en", "input":string[i]}
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            response = json.loads(response.text)
+            string[i] = response['outputs'][0]['output'].lower().lstrip(" ").lstrip("the")
         try:
-            smi = str(smiles(get_compounds(element, "name")[0].canonical_smiles))
+            smi = str(smiles(get_compounds(string[i], "name")[0].canonical_smiles))
             smiles_list.append(smi)
         except IndexError:
             continue
--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,18 @@
-document.onload = function () {
-	// Initializing variables
-    var imageField = document.getElementById("imageField");
-	var mainText = document.getElementById("mainText");
-	var beamSizeValue = document.getElementById("beamSizeValue");
-};
+var imageField = document.getElementById("imageField");
+var mainText = document.getElementById("mainText");
+var predField = document.getElementById("prodSmi");
+var beamSizeValue = document.getElementById("beamSizeValue");
+var langValue = document.getElementById("selectLang");
+
+var selectLang = document.getElementById("selectLang");
+var languages = ["ru", "en", "fr", "de"];
+for (var i = 0; i < languages.length; i++) {
+	var opt = languages[i];
+	var elem = document.createElement("option");
+	elem.textContent = opt.toUpperCase();
+	elem.value = opt;
+	selectLang.appendChild(elem);
+}
 
 function getImage() {
 	// Get depiction of SMILES
@@ -18,7 +27,7 @@ function getImage() {
 };
 
 function getPrediction() {
-	var predField = document.getElementById("prodSmi");
+	// Get Prediction (Algorithmia pipeline)
 	$.getJSON(
 		'/predict', 
 		{ post: mainText.value,
@@ -38,7 +47,8 @@ function getSMILES() {
 	// Get SMILES string for names
 	$.getJSON(
 		'/smiles', 
-		{ post: mainText.value }, 
+		{ post: mainText.value ,
+		  lang: langValue.value }, 
 		function(data) {
 			var response = data.smiles;
 			var image = data.img; 
